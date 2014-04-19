c3462d51173379e10fa18389581456c96663c9b4
Colin Shea <colin.shea@evogi.com>
2014-04-19 03:44:26 -0700

    A user can now edit their password
    Re-enable only a partial bit of the Registerable Devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -39,5 +39,9 @@
 Fisheye2::Application.routes.draw do
   root to: 'home#index'
   devise_for :users, skip: :registrations
+  devise_scope :user do
+    get 'users/edit' => 'devise/registrations#edit', as: 'edit_user_registration'
+    patch 'users/:id' => 'devise/registrations#update', as: 'user_registration'
+  end
   resources :users
 end
