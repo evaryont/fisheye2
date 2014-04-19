6597a59a873033439d5326442850c5c18c6fe82c
Colin Shea <colin.shea@evogi.com>
2014-04-19 01:41:12 -0700

    Convert specs to RSpec 2.14.8 syntax with Transpec
    This conversion is done by Transpec 1.13.0 with the following command:
        transpec
    
    * 8 conversions
        from: obj.should_not
          to: expect(obj).not_to
    
    * 7 conversions
        from: obj.should
          to: expect(obj).to
    
    * 1 conversion
        from: == expected
          to: eq(expected)

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -5,7 +5,7 @@ describe HomeController do
   describe 'unauthenticated' do
     it 'should redirect to login_path' do
       get 'index'
-      response.should redirect_to(new_user_session_path)
+      expect(response).to redirect_to(new_user_session_path)
     end
   end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -39,14 +39,14 @@ describe User do
 
   it 'should require an email address' do
     no_email_user = build(:user, email: '')
-    no_email_user.should_not be_valid
+    expect(no_email_user).not_to be_valid
   end
 
   it 'should accept valid email addresses' do
     addresses = %w(user@foo.com THE_USER@foo.bar.org first.last@foo.jp)
     addresses.each do |address|
       valid_email_user = create(:user, email: address)
-      valid_email_user.should be_valid
+      expect(valid_email_user).to be_valid
     end
   end
 
@@ -54,7 +54,7 @@ describe User do
     addresses = %w(user@foo,com user_at_foo.org example.user@foo.)
     addresses.each do |address|
       invalid_email_user = build(:user, email: address)
-      invalid_email_user.should_not be_valid
+      expect(invalid_email_user).not_to be_valid
     end
   end
 
@@ -62,40 +62,40 @@ describe User do
     create(:user)
 
     user_with_duplicate_email = build(:user)
-    user_with_duplicate_email.should_not be_valid
+    expect(user_with_duplicate_email).not_to be_valid
   end
 
   it 'should reject duplicate email addresses regardless of case' do
     create(:user, email: 'USER@EXAMPLE.COM')
 
     user_with_duplicate_email = build(:user, email: 'user@example.com')
-    user_with_duplicate_email.should_not be_valid
+    expect(user_with_duplicate_email).not_to be_valid
   end
 
   describe 'passwords' do
     let(:user) { create(:user) }
 
     it 'should have a password attribute' do
-      user.should respond_to(:password)
+      expect(user).to respond_to(:password)
     end
 
     it 'should have a password confirmation attribute' do
-      user.should respond_to(:password_confirmation)
+      expect(user).to respond_to(:password_confirmation)
     end
   end
 
   describe 'password validations' do
     it 'should require a password' do
-      build(:user, password: '', password_confirmation: '').should_not be_valid
+      expect(build(:user, password: '', password_confirmation: '')).not_to be_valid
     end
 
     it 'should require a matching password confirmation' do
-      build(:user, password_confirmation: '').should_not be_valid
+      expect(build(:user, password_confirmation: '')).not_to be_valid
     end
 
     it 'should reject short passwords' do
       short = 'a' * 5
-      build(:user, password: short, password_confirmation: short).should_not be_valid
+      expect(build(:user, password: short, password_confirmation: short)).not_to be_valid
     end
   end
 
@@ -103,11 +103,11 @@ describe User do
     let(:user) { create(:user) }
 
     it 'should have an encrypted password attribute' do
-      user.should respond_to(:encrypted_password)
+      expect(user).to respond_to(:encrypted_password)
     end
 
     it 'should set the encrypted password attribute' do
-      user.encrypted_password.should_not be_blank
+      expect(user.encrypted_password).not_to be_blank
     end
   end
 end
